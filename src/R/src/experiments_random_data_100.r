89181b2c5fdcaaa685a9dd35d5874e3bc2ee64d9
Atanas Dimitrov <atanas_sz@abv.bg>
2019-08-12 15:28:38 +0200

    little refactoring and generation of mean absolute error .cvs files

---
+++ src/R/src/experiments_random_data_100.r
@@ -0,0 +1,13 @@
+source("install_required_packages.r")
+source("synth_validation_benchmark.r")
+
+library(tictoc)
+
+tic("total")
+for(i in 1:15){
+  tic(paste("random_generated_data_",i,sep = ""))
+  benchmark(NULL,100,100,FALSE,TRUE)
+  toc()
+}
+toc()
+
---
+++ src/R/src/install_required_packages.r
@@ -0,0 +1,38 @@
+## Install required packages =============================================================
+
+#install cran packages
+packages = c("tictoc","assertthat","psych","collections","MASS","NlcOptim","mltools","data.table","devtools","grf","gridExtra")
+packagecheck <- match( packages, utils::installed.packages()[,1] )
+
+packagestoinstall <- packages[ is.na( packagecheck ) ]
+
+if( length( packagestoinstall ) > 0L ) {
+  utils::install.packages( packagestoinstall,
+                           repos = "http://cran.us.r-project.org"
+  )
+} else {
+  print( "All required cran packages already installed" )
+}
+#finish installing cran packages
+
+#install packages from github
+packages_from_github_names<-c("rlearner")
+packages_from_github_repos<-c("xnie/rlearner")
+
+packagecheck <- match( packages_from_github_names, utils::installed.packages()[,1] )
+
+packagestoinstall <- packages_from_github_repos[ is.na( packagecheck ) ]
+
+if( length( packagestoinstall ) > 0L ) {
+  library(devtools)
+  
+  for(p in packagestoinstall){
+    install_github(p)
+  }
+  
+} else {
+  print( "All required github packages already installed" )
+}
+#finish installing packages from github
+
+## Finish installing required packages =======================================================
