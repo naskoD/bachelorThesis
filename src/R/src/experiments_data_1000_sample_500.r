9dbfd26579fb3702fe2db2dc431b77b74a57de98
Atanas Dimitrov <atanas_sz@abv.bg>
2019-08-25 19:28:23 +0200

    two new experiments added

---
+++ src/R/src/experiments_data_1000_sample_500.r
@@ -0,0 +1,13 @@
+source("install_required_packages.r")
+source("synth_validation_benchmark.r")
+
+library(tictoc)
+
+tic("total")
+for(i in 1:15){
+  tic(paste("real_data_raw/data_1000_1_run",i,sep = ""))
+  benchmark("real_data_raw/data_1000_1",500,100,FALSE,TRUE)
+  toc()
+}
+toc()
+
