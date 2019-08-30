b66b3795a07a5e667b93428057570c31358ca41f
Atanas Dimitrov <atanas_sz@abv.bg>
2019-08-30 07:00:59 +0200

    theoretical part of the thesis finished

--- src/R/src/experiments_data_1000_sample_200.r
+++ src/R/src/experiments_data_1000_sample_200.r
@@ -6,7 +6,7 @@ library(tictoc)
 tic("total")
 for(i in 1:15){
   tic(paste("real_data_raw/data_1000_1_run",i,sep = ""))
-  benchmark("real_data_raw/data_1000_1",500,100,FALSE,TRUE)
+  benchmark("real_data_raw/data_1000_1",200,100,FALSE,TRUE)
   toc()
 }
 toc()
